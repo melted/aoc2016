7a9cebd300dba8e14e8abfd59de6ded5f24dd44c
Niklas Larsson <niklas@mm.st>
2016-12-05 11:36:23 +0100

    Day 5 -- part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,24 +1,38 @@
 extern crate md5;
 
-use md5::*;
-
-
 fn main() {
     let data = "wtnhxymk";
     let mut password = String::new();
     let mut index = 0;
 
     while password.len() < 8 {
-        loop {
-            let s = format!("{}{}", data, index);
-            let digest = md5::compute(s.as_bytes());
-            index = index + 1;
-            if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
-                println!("----{:?}: {:?}", s, digest);
-                password.push_str(&format!("{:x}", digest[2] & 0x0f));
-                break;
-            }
-        }        
+        let s = format!("{}{}", data, index);
+        let digest = md5::compute(s.as_bytes());
+        index = index + 1;
+        if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
+            password.push_str(&format!("{:x}", digest[2] & 0x0f));
+        }
     }
+
     println!("{}", password);
+
+    let mut password2 : [u8; 8]= [255; 8];
+    let mut found = 0;
+    index = 0;
+
+    while found < 8 {
+        let s = format!("{}{}", data, index);
+        let digest = md5::compute(s.as_bytes());
+        index = index + 1;
+        if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
+            let pos = digest[2] & 0x0f;
+            let val = (digest[3] & 0xf0) >> 4;
+            if pos < 8 && password2[pos as usize] == 255 {
+                found = found + 1;
+                password2[pos as usize] = val;
+            }
+        }
+    }
+
+    println!("{:?}", password2);
 }
