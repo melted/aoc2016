ae093fef27723129d7091093fc18b8f3ad85bea7
Niklas Larsson <niklas@mm.st>
2016-12-05 19:25:07 +0100

    Run part 1 and 2 simulataneously.

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -4,35 +4,27 @@ fn main() {
     let data = "wtnhxymk";
     let mut password = String::new();
     let mut index = 0;
-
-    while password.len() < 8 {
-        let s = format!("{}{}", data, index);
-        let digest = md5::compute(s.as_bytes());
-        index = index + 1;
-        if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
-            password.push_str(&format!("{:x}", digest[2] & 0x0f));
-        }
-    }
-
-    println!("{}", password);
-
-    let mut password2 : [u8; 8]= [255; 8];
+    let mut password2 : [u8; 8] = [255; 8];
     let mut found = 0;
-    index = 0;
 
     while found < 8 {
         let s = format!("{}{}", data, index);
         let digest = md5::compute(s.as_bytes());
         index = index + 1;
         if digest[0] == 0 && digest[1] == 0 && digest[2] & 0xf0 == 0 {
-            let pos = digest[2] & 0x0f;
-            let val = (digest[3] & 0xf0) >> 4;
-            if pos < 8 && password2[pos as usize] == 255 {
+            if password.len() < 8 {
+                password.push_str(&format!("{:x}", digest[2] & 0x0f));
+            }
+            let pos = (digest[2] & 0x0f) as usize;
+            if pos < 8 && password2[pos] == 255 {
                 found = found + 1;
-                password2[pos as usize] = val;
+                password2[pos] = (digest[3] & 0xf0) >> 4;
             }
         }
     }
-
-    println!("{:?}", password2);
+    let mut pw2 = String::new();
+    for x in password2.iter() {
+        pw2.push_str(&format!("{:x}", x));
+    }
+    println!("{} {}", password, pw2);
 }
