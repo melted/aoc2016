358efc8bb4d9444264190e67d76e7ab41aff1717
Niklas Larsson <niklas@mm.st>
2016-12-11 22:22:17 +0100

    Clean up a bit

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,28 +1,15 @@
 use std::collections::HashSet;
 
-#[derive(Debug, Eq, Hash)]
+#[derive(Debug, PartialEq, Eq, Hash)]
 struct Config {
     chips : Vec<u32>,
     generators : Vec<u32>,
     elevator : u32,
 }
 
-impl PartialEq for Config {
-     fn eq(&self, other:&Config) -> bool {
-         for i in 0..self.chips.len() {
-             if self.chips[i] != other.chips[i] ||
-                self.generators[i] != other.generators[i] {
-                    return false
-                }
-         }
-         self.elevator == other.elevator
-     }
-}
-
 fn is_legal(c: &Config) -> bool {
     for (i, floor) in c.chips.iter().enumerate() {
         if c.generators[i] != *floor {
-            // unprotected chip
             if c.generators.iter().any(|f| f == floor) {
                 return false;
             }
@@ -43,7 +30,6 @@ fn possible_moves(c : &Config) -> Vec<Config> {
     let mut out = Vec::new();
     for i in 0..c.chips.len() {
         if c.chips[i] == c.elevator {
-            // Carry just that
             if c.elevator < 3 {
                 let mut up = copy_config(c);
                 up.chips[i] += 1;
@@ -63,7 +49,6 @@ fn possible_moves(c : &Config) -> Vec<Config> {
                 }
                 out.push(up);
             }
-
             if c.elevator > 0 {
                 let mut down = copy_config(c);
                 down.chips[i] -= 1;
@@ -85,7 +70,6 @@ fn possible_moves(c : &Config) -> Vec<Config> {
             }
         }
         if c.generators[i] == c.elevator {
-              // Carry just that
             if c.elevator < 3 {
                 let mut up = copy_config(c);
                 up.generators[i] += 1;
@@ -100,7 +84,6 @@ fn possible_moves(c : &Config) -> Vec<Config> {
                 }
                 out.push(up);
             }
-
             if c.elevator > 0 {
                 let mut down = copy_config(c);
                 down.generators[i] -= 1;
@@ -138,7 +121,6 @@ fn search(init : Config) -> u32 {
     let mut visited = HashSet::new();
     let mut rounds = 0;
     candidates.insert(init);
-
     loop {
         rounds += 1;
         let mut next_round = HashSet::new();
@@ -147,10 +129,7 @@ fn search(init : Config) -> u32 {
             visited.insert(normalize(&conf));
             for m in moves {
                 let s = normalize(&m);
-                if is_legal(&s) {
-                    if visited.contains(&s) {
-                        continue;
-                    }
+                if is_legal(&s) && !visited.contains(&s) {
                     if is_finished(&s) {
                         return rounds;
                     }
@@ -162,11 +141,9 @@ fn search(init : Config) -> u32 {
     }
 }
 
-
 fn main() {
     let start = Config { chips: vec![0,2,2,2,2], generators: vec![0,1,1,1,1], elevator: 0 };
     let start2 = Config { chips: vec![0,2,2,2,2,0,0], generators: vec![0,1,1,1,1,0,0], elevator: 0 };
-    
     let moves = search(start);
     let moves2 = search(start2);
     println!("{} {}", moves, moves2);
